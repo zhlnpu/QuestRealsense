ab343cf083c4da2706eefc53f5a1522f4957c178
Li Zhang <zhlnpu@gmail.com>
2021-04-06 19:35:40 +0800

    add new version

---
+++ Assets/AndroidPermission.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AndroidPermission : MonoBehaviour
+{
+#if UNITY_ANDROID && !UNITY_EDITOR
+    void Awake()
+    {
+        if (!UnityEngine.Android.Permission.HasUserAuthorizedPermission(UnityEngine.Android.Permission.Camera))
+        {
+            UnityEngine.Android.Permission.RequestUserPermission(UnityEngine.Android.Permission.Camera);
+
+        }
+
+        using (var javaUnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
+        using (var rsContext = new AndroidJavaClass("com.intel.realsense.librealsense.RsContext"))
+        {
+            Debug.Log(rsContext);
+            rsContext.CallStatic("init", currentActivity);
+        }
+    }
+#endif
+}
